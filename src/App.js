c4c060023afe6582a033d3b01a44e020c650ee71
bhageerath-reddy <bhageerath30@gmail.com>
2025-01-24 11:27:50 -0500

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AOS from 'aos';
 import "aos/dist/aos.css";
 import './index.css';
 import {
-  BrowserRouter as Router,
+  HashRouter,
   Routes,
   Route
 } from 'react-router-dom';
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <HashRouter>
         <ScrollToTop>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -42,7 +42,7 @@ function App() {
             <Route path="/get-demo" element={<DemoProduct />} /> 
           </Routes>
         </ScrollToTop>
-      </Router>
+        </HashRouter>
     </>
   );
 }
